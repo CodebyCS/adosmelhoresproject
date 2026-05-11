6fd4e70cb582e25dd9b52d156d315d652af05429
GuiSouza95 <guilhermesouzaa2018@gmail.com>
2026-05-11 20:21:55 +0100

    Adicionado o método de CalcularPagamentoFormador

--- src/Models/Formador.cs
+++ src/Models/Formador.cs
@@ -5,5 +5,14 @@ namespace adosmelhoresproject.src.Models
         public string AreaLecionada { get; set; }
         public string Disponibilidade { get; set; }
         public decimal ValorHora { get; set; }
+
+        public decimal CalcularPagamentoFormador(DateTime dataInicio, DateTime dataFim)
+        {
+            int Dias = (dataFim - dataInicio).Days + 1;
+
+            decimal valorTotal = Dias * 6 * ValorHora;
+            
+            return valorTotal;
+        }
     }
 }
