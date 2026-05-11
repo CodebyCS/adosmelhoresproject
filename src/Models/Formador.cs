4f5a5aa0db8dca27d2e22fe69a9d052b75f068b9
Vi-XavierSantos <xavysantos1@gmail.com>
2026-05-11 20:45:12 +0100

    adicionado suporte a polimorfismo na classe Funcionario para persistência Json

---
+++ src/Models/Formador.cs
@@ -0,0 +1,18 @@
+namespace adosmelhoresproject.src.Models
+{
+    public class Formador : Funcionario
+    {
+        public string AreaLecionada { get; set; }
+        public string Disponibilidade { get; set; }
+        public decimal ValorHora { get; set; }
+
+        public decimal CalcularPagamentoFormador(DateTime dataInicio, DateTime dataFim)
+        {
+            int Dias = (dataFim - dataInicio).Days + 1;
+
+            decimal valorTotal = Dias * 6 * ValorHora;
+            
+            return valorTotal;
+        }
+    }
+}
