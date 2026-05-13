a86dc092d5ea88b98feef18b8b92504df5aa1c3f
emersonr02 <254676587+emersonr02@users.noreply.github.com>
2026-05-13 19:53:29 +0100

    Create transacao.cs

---
+++ src/Models/Transacao.cs
@@ -0,0 +1,12 @@
+﻿namespace adosmelhoresproject.src.Models;
+
+public enum TipoTransacao { Receita, Despesa }
+
+public class Transacao
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public DateTime Data { get; set; }
+    public decimal Valor { get; set; }
+    public TipoTransacao Tipo { get; set; }
+    public string Descricao { get; set; } = string.Empty;
+}
