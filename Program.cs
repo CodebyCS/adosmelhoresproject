0deeb3c0b780162e6cc056f4a9062df7f7ac143f
Carlos Santana <codebycs@gmail.com>
2026-05-13 21:48:02 +0100

    Refactor: project

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-using adosmelhoresproject.src.Components;
 using adosmelhoresproject.src.Models;
-using adosmelhoresproject.src.Components;
 using adosmelhoresproject.src.Interfaces;
 using adosmelhoresproject.src.Services;
 
@@ -30,14 +28,11 @@ namespace adosmelhoresproject
                 app.UseHsts();
             }
 
-            app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+            
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseAntiforgery();
-
-            app.MapRazorComponents<App>()
-                .AddInteractiveServerRenderMode();
 
             app.Run();
         }
