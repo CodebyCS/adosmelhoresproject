bded36d06ff8dfd8343365b084905232a8511e18
emersonr02 <254676587+emersonr02@users.noreply.github.com>
2026-05-13 20:09:41 +0100

    Feat: Add ITransacaoService e TransacaoService

---
+++ src/Interfaces/ITransacaoService.cs
@@ -0,0 +1,10 @@
+﻿using adosmelhoresproject.src.Models;
+namespace adosmelhoresproject.src.Interfaces
+{
+    public interface ITransacaoService
+    {
+        List<Transacao> GetAll();
+        void Adicionar(Transacao transacao);
+        List<Transacao> GetPorMes(DateTime inicio, DateTime Fim);
+    }
+}
---
+++ src/Services/TransacaoService.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using adosmelhoresproject.src.Models;
+using adosmelhoresproject.src.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+
+namespace adosmelhoresproject.src.Services
+{
+    public class TransacaoService
+    {
+
+    }
+}
