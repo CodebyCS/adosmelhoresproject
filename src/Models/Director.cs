9797590e243b790e771a8c83384196f4c8a54803
GuiSouza95 <guilhermesouzaa2018@gmail.com>
2026-05-13 22:20:23 +0100

    Refatorado a função de calcular salário

--- src/Models/Director.cs
+++ src/Models/Director.cs
@@ -5,5 +5,19 @@ namespace adosmelhoresproject.src.Models
         public bool HoursExemption { get; set; }
         public int MonthlyBonus { get; set; }
         public bool CompanyCar { get; set; }
+
+        public override void CalculateTrainerPayment(DateTime startDate, DateTime endDate)
+        {
+            decimal baseValue = 3000;
+
+            if (CompanyCar)
+            {
+                this.Salary = baseValue + MonthlyBonus + 500;
+            }
+            else
+            {            
+                this.Salary = baseValue + MonthlyBonus;
+            }
+        }
     }
 }
