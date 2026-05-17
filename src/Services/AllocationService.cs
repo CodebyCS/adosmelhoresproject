18d3e8590c650e72ad8696778278e1bf67b47527
codebycs <codebycs@gmail.com>
2026-05-17 15:10:16 +0100

    feat: AllocationService

---
+++ src/Services/AllocationService.cs
@@ -0,0 +1,20 @@
+﻿using adosmelhoresproject.src.Interfaces;
+using adosmelhoresproject.src.Models;
+
+namespace adosmelhoresproject.src.Services
+{
+    public class AllocationService : JsonRepository<Allocation>, IAllocationService
+    {
+        public AllocationService(IWebHostEnvironment env)
+            : base(env, "alocacoes.json") { }
+
+        public List<Allocation> GetAll() => ReadAll();
+
+        public void Add(Allocation a)
+        {
+            var list = ReadAll();
+            list.Add(a);
+            WriteAll(list);
+        }
+    }
+}
